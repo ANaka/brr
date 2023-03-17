07225f913fbc2b95fd6547a2b271337fef38a401
Naka <alex.naka@gmail.com>
2023-03-17 11:10:38 -0700

    switch to half-edge data structure but it breaks stuff a bit

--- libraries/geodude/geodude/deform.py
+++ libraries/geodude/geodude/deform.py
@@ -1,52 +1,76 @@
+from typing import List
+
 import numpy as np
-from itertools import product
 from shapely import MultiLineString
+
+
+class Vertex:
+    def __init__(self, position: np.ndarray):
+        self.position = position
+        self.half_edge = None
+
+
+class HalfEdge:
+    def __init__(self, rest_length: float):
+        self.vertex = None
+        self.opposite = None
+        self.next = None
+        self.rest_length = rest_length
+
+
 class Cloth:
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int, rest_length: float = 1.0):
         self.width = width
         self.height = height
-        self.vertices = np.zeros((height, width, 2), dtype=np.float64)
+
+        # Initialize vertices and half-edges
+        self.vertices = [[Vertex(np.array([j, i], dtype=np.float64)) for j in range(width)] for i in range(height)]
+        self.half_edges = [[HalfEdge(rest_length) for j in range(width)] for i in range(height)]
+
+        # Connect vertices and half-edges
         for i in range(height):
             for j in range(width):
-                self.vertices[i, j] = np.array([j, i], dtype=np.float64)
-        self.edges = self._initialize_edges()
+                self.vertices[i][j].half_edge = self.half_edges[i][j]
+                self.half_edges[i][j].vertex = self.vertices[i][j]
 
-    def _initialize_edges(self):
-        edges = []
-        for ii, jj in product(range(self.height), range(self.width)):
-            if jj < self.width - 1:
-                edges.append((self.vertices[ii, jj], self.vertices[ii, jj + 1]))
-            if ii < self.height - 1:
-                edges.append((self.vertices[ii, jj], self.vertices[ii + 1, jj]))
-        return edges
+                # Connect half-edges horizontally
+                if j < width - 1:
+                    self.half_edges[i][j].next = self.half_edges[i][j + 1]
+                    self.half_edges[i][j + 1].opposite = self.half_edges[i][j]
+
+                # Connect half-edges vertically
+                if i < height - 1:
+                    self.half_edges[i][j].opposite = self.half_edges[i + 1][j]
+                    self.half_edges[i + 1][j].next = self.half_edges[i][j]
 
     def apply_gravity(self, gravity: float):
         for i in range(self.height):
             for j in range(self.width):
-                self.vertices[i, j] -= np.array([0, gravity])
+                self.vertices[i][j].position -= np.array([0, gravity])
 
     def apply_wind(self, wind_vector: np.ndarray):
         for i in range(self.height):
             for j in range(self.width):
-                self.vertices[i, j] += wind_vector
+                self.vertices[i][j].position += wind_vector
 
     def update(self, time_step: float, gravity: float, wind_vector: np.ndarray):
         self.apply_gravity(gravity * time_step)
         self.apply_wind(wind_vector * time_step)
         self.satisfy_constraints(1)
 
-    def satisfy_constraints(self, iterations: int):
+    def satisfy_constraints(self, iterations: int = 1):
         for _ in range(iterations):
             for i in range(self.height):
                 for j in range(self.width):
-                    if j < self.width - 1:
-                        self.vertices[i, j], self.vertices[i, j + 1] = self._satisfy_constraint(
-                            self.vertices[i, j], self.vertices[i, j + 1], 1
-                        )
-                    if i < self.height - 1:
-                        self.vertices[i, j], self.vertices[i + 1, j] = self._satisfy_constraint(
-                            self.vertices[i, j], self.vertices[i + 1, j], 1
-                        )
+                    half_edge = self.half_edges[i][j]
+                    p1 = half_edge.vertex.position
+                    if half_edge.next is not None:
+                        p2 = half_edge.next.vertex.position
+                        delta = p2 - p1
+                        distance = np.linalg.norm(delta)
+                        difference = (half_edge.rest_length - distance) / distance
+                        half_edge.vertex.position += delta * 0.5 * difference
+                        half_edge.next.vertex.position -= delta * 0.5 * difference
 
     def _satisfy_constraint(self, p1: np.ndarray, p2: np.ndarray, rest_length: float):
         delta = p2 - p1
@@ -59,10 +83,29 @@ class Cloth:
     def handle_collision(self, sphere_center: np.ndarray, sphere_radius: float):
         for i in range(self.height):
             for j in range(self.width):
-                vertex_to_center = self.vertices[i, j] - sphere_center
+                vertex_to_center = self.vertices[i][j].position - sphere_center
                 distance = np.linalg.norm(vertex_to_center)
                 if distance < sphere_radius:
-                    self.vertices[i, j] = sphere_center + vertex_to_center / distance * sphere_radius
+                    self.vertices[i][j].position = sphere_center + vertex_to_center / distance * sphere_radius
+
+    def get_vertex_positions(self) -> np.ndarray:
+        positions = np.zeros((self.height, self.width, 2))
+        for i in range(self.height):
+            for j in range(self.width):
+                positions[i, j] = self.vertices[i][j].position
+        return positions
+
+    def get_edge_lengths(self) -> List[float]:
+        edge_lengths = []
+        for i in range(self.height):
+            for j in range(self.width):
+                half_edge = self.half_edges[i][j]
+                if half_edge.next is not None:
+                    p1 = half_edge.vertex.position
+                    p2 = half_edge.next.vertex.position
+                    length = np.linalg.norm(p2 - p1)
+                    edge_lengths.append(length)
+        return edge_lengths
 
     def to_multilinestring(self) -> MultiLineString:
         """Convert the cloth's geometry into a shapely MultiLineString for plotting."""
@@ -71,11 +114,17 @@ class Cloth:
         # Add horizontal lines
         for i in range(self.height):
             for j in range(self.width - 1):
-                lines.append((tuple(self.vertices[i, j]), tuple(self.vertices[i, j + 1])))
+                lines.append((tuple(self.vertices[i][j].position), tuple(self.vertices[i][j + 1].position)))
 
         # Add vertical lines
         for i in range(self.height - 1):
             for j in range(self.width):
-                lines.append((tuple(self.vertices[i, j]), tuple(self.vertices[i + 1, j])))
+                lines.append((tuple(self.vertices[i][j].position), tuple(self.vertices[i + 1][j].position)))
 
         return MultiLineString(lines)
+
+    def jitter_vertices(self, jitter_amount: float):
+        for i in range(self.height):
+            for j in range(self.width):
+                random_offset = np.random.uniform(-jitter_amount, jitter_amount, 2)
+                self.vertices[i][j].position += random_offset
--- libraries/geodude/tests/test_deform.py
+++ libraries/geodude/tests/test_deform.py
@@ -1,6 +1,6 @@
 import numpy as np
 from geodude.deform import Cloth
-from itertools import product
+
 
 def test_cloth_initialization():
     width = 10
@@ -10,34 +10,34 @@ def test_cloth_initialization():
     assert isinstance(cloth, Cloth), "Cloth object not created correctly"
     assert cloth.width == width, "Cloth width not initialized correctly"
     assert cloth.height == height, "Cloth height not initialized correctly"
-    assert cloth.vertices.shape == (height, width, 2), "Cloth vertices not initialized correctly"
-    assert len(cloth.edges) == (width - 1) * height + width * (height - 1), "Cloth edges not initialized correctly"
+    assert len(cloth.vertices) == height, "Cloth vertices not initialized correctly"
+    assert len(cloth.vertices[0]) == width, "Cloth vertices not initialized correctly"
 
 
 def test_apply_gravity():
     cloth = Cloth(5, 5)
-    initial_vertices = np.copy(cloth.vertices)
+    initial_vertices = np.copy(cloth.get_vertex_positions())
     cloth.apply_gravity(0.5)
 
     for i in range(cloth.height):
         for j in range(cloth.width):
-            assert cloth.vertices[i, j][1] == initial_vertices[i, j][1] - 0.5, "Gravity not applied correctly"
+            assert cloth.vertices[i][j].position[1] == initial_vertices[i, j][1] - 0.5, "Gravity not applied correctly"
 
 
 def test_apply_wind():
     cloth = Cloth(5, 5)
-    initial_vertices = np.copy(cloth.vertices)
+    initial_vertices = np.copy(cloth.get_vertex_positions())
     wind_vector = np.array([1.0, 0.0])
     cloth.apply_wind(wind_vector)
 
     for i in range(cloth.height):
         for j in range(cloth.width):
-            assert cloth.vertices[i, j][0] == initial_vertices[i, j][0] + 1.0, "Wind not applied correctly"
+            assert cloth.vertices[i][j].position[0] == initial_vertices[i, j][0] + 1.0, "Wind not applied correctly"
 
 
 def test_update_cloth():
     cloth = Cloth(5, 5)
-    initial_vertices = np.copy(cloth.vertices)
+    initial_vertices = np.copy(cloth.get_vertex_positions())
     gravity = 0.5
     wind_vector = np.array([1.0, 0.0])
     time_step = 1.0
@@ -46,23 +46,23 @@ def test_update_cloth():
     for i in range(cloth.height):
         for j in range(cloth.width):
             assert (
-                cloth.vertices[i, j][0] == initial_vertices[i, j][0] + wind_vector[0]
+                cloth.vertices[i][j].position[0] == initial_vertices[i, j][0] + wind_vector[0]
             ), "Cloth update not working correctly"
-            assert cloth.vertices[i, j][1] == initial_vertices[i, j][1] - gravity, "Cloth update not working correctly"
+            assert (
+                cloth.vertices[i][j].position[1] == initial_vertices[i, j][1] - gravity
+            ), "Cloth update not working correctly"
 
 
 def test_cloth_constraints():
     cloth = Cloth(4, 4)
-    initial_edges = cloth.edges.copy()
+    initial_edges = cloth.get_edge_lengths()
     cloth.apply_gravity(1)
     cloth.satisfy_constraints(10)
     tolerance = 0.01
+    updated_edges = cloth.get_edge_lengths()
 
-    for edge in cloth.edges:
-        p1, p2 = edge
-        length = np.linalg.norm(p2 - p1)
-        assert np.abs(length - 1) < tolerance
-
+    for i in range(len(initial_edges)):
+        assert np.abs(updated_edges[i] - initial_edges[i]) < tolerance, f"Edge {i} not satisfied"
 
 
 def test_cloth_collision_response():
@@ -74,5 +74,5 @@ def test_cloth_collision_response():
     # Check if the cloth vertices are outside the sphere
     for i in range(cloth.height):
         for j in range(cloth.width):
-            dist_to_center = np.linalg.norm(cloth.vertices[i, j] - sphere_center)
+            dist_to_center = np.linalg.norm(cloth.vertices[i][j].position - sphere_center)
             assert dist_to_center >= sphere_radius - 1e-05, "Cloth collision response not working correctly"
