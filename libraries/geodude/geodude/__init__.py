ec9860466fecb19469c2944e1720a7e9f612cd14
Alex Naka <alex.naka@gmail.com>
2023-03-21 18:18:39 -0700

    updates to storage

---
+++ libraries/geodude/geodude/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+geodude_root = Path(__file__).parent.parent
+libraries_root = geodude_root.parent
+repo_root = libraries_root.parent
+outputs_root = repo_root / "outputs"
